8e03b0585b088905168c39e056bf994bc7aa53d9
yyokii <yyokii.h@gmil.com>
2017-12-30 07:36:33 +0900

    popoverの実装テスト

---
+++ EngineerSocialApp/Controller/PopOverContentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopOverContentViewController.swift
+//  EngineerSocialApp
+//
+//  Created by Yoki on 2017/12/26.
+//
+
+import UIKit
+
+class PopOverContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- EngineerSocialApp/Controller/PostVC.swift
+++ EngineerSocialApp/Controller/PostVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PostVC: UIViewController {
+class PostVC: UIViewController, UIPopoverPresentationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +21,26 @@ class PostVC: UIViewController {
     
     @IBAction func closeTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tapModalPresentationStyle(_ sender: UIButton) {
+        
+        //let contentVC = PopOverContentViewController()
+        let storyBoard = UIStoryboard(name: "PopOver", bundle: nil)
+        let contentVC = storyBoard.instantiateInitialViewController() as! PopOverContentViewController
+        contentVC.modalPresentationStyle = .popover
+        contentVC.preferredContentSize = CGSize(width: 300, height: 300)
+        //sourceViewは表示するviewを指定して、sourceRectはそのviewの中のどこからにゅにゅっと表示するかを指定する
+        contentVC.popoverPresentationController?.sourceView = view
+        contentVC.popoverPresentationController?.sourceRect = sender.frame
+        contentVC.popoverPresentationController?.permittedArrowDirections = .any
+        contentVC.popoverPresentationController?.delegate = self
+        present(contentVC, animated: true, completion: nil)
+    }
+    
+    /// Popover appears on iPhone
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return .none
     }
     
 }
